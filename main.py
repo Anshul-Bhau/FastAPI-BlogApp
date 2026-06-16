b7c07cf32d2f3cd1661cc3a443092396111a78d6
Anshul-Bhau <anshulbhau1@gmail.com>
2026-06-16 21:40:21 +0530

    Implemented patch method for users

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from schemas import PostCreate, PostResponse,PostUpdate, UserCreate, UserResponse
+from schemas import PostCreate, PostResponse,PostUpdate, UserCreate, UserResponse, UserUpdate
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -147,6 +147,49 @@ def get_user(user_id : int, db:Annotated[Session, Depends(get_db)]):
         detail="User not found"
     )
 
+@app.patch("api/users/{user_id}", response_model=UserResponse)
+def update_user(
+    user_id : int,
+    user_update: UserUpdate,
+    db: Annotated[Session, Depends(get_db)]
+):
+    result = db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail = "User not found"
+        )
+    
+    if user_update.username is not None and user_update.username != user.username:
+        result = db.execute(select(models.User).where(models.User.username == user_update.username))
+        existing_user = result.scalars().first()
+        if existing_user:
+            raise HTTPException(
+                status_code= status.HTTP_400_BAD_REQUEST,
+                detail="Username already exists"
+            )
+
+    if user_update.email is not None and user_update.email != user.email:
+        result = db.execute(select(models.User).where(models.User.email == user_update.email))
+        existing_user = result.scalars().first()
+        if existing_user:
+            raise HTTPException(
+                status_code= status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered"
+            )
+    
+    if user_update.username is not None:
+        user.username = user_update.username
+    if user_update.email is not None:
+        user.email = user_update.email
+    if user_update.image_file is not None:
+        user.image_file = user_update.image_file
+
+    db.commit()
+    db.refresh(user)
+    return user
+
 @app.get("/api/users/{user_id}/posts", response_model=list[PostResponse])
 def get_user_posts(user_id: int, db: Annotated[Session, Depends(get_db)]):
     result = db.execute(select(models.User).where(models.User.id == user_id))
@@ -175,7 +218,6 @@ def get_post(post_id : int, db:Annotated[Session, Depends(get_db)]):
     if post:
         return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-
 
 
 @app.post(
